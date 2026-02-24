aa24213500d20c43f7dbe87e7030402729c810ba
zz2548 <zz2548@nyu.edu>
2026-02-24 11:57:49 -0500

    wall/floor variants

--- pcg-game/scripts/main.gd
+++ pcg-game/scripts/main.gd
@@ -20,10 +20,28 @@ func _ready():
 
 func _render_tilemap():
 	tilemap.clear()
+
+	var floor_variants = [
+		Vector2i(2,2), Vector2i(3,2), Vector2i(4,2),
+		Vector2i(2,3), Vector2i(3,3), Vector2i(4,3),
+		Vector2i(2,4), Vector2i(3,4), Vector2i(4,4),
+	]
+	var wall_variants = [
+		Vector2i(1,1), Vector2i(2,1), Vector2i(3,1), Vector2i(4,1), Vector2i(5,1),
+		Vector2i(1,2), Vector2i(5,2),
+		Vector2i(1,3), Vector2i(5,3),
+		Vector2i(1,4), Vector2i(5,4),
+		Vector2i(1,5), Vector2i(2,5), Vector2i(3,5), Vector2i(4,5), Vector2i(5,5),
+	]
+
 	for y in generator.height:
 		for x in generator.width:
-			var atlas = FLOOR_ATLAS if generator.grid[y][x] == generator.FLOOR else WALL_ATLAS
-			tilemap.set_cell(0, Vector2i(x, y), 0, atlas)
+			if generator.grid[y][x] == generator.FLOOR:
+				var atlas = floor_variants[randi() % floor_variants.size()]
+				tilemap.set_cell(0, Vector2i(x, y), 0, atlas)
+			else:
+				var atlas = wall_variants[randi() % wall_variants.size()]
+				tilemap.set_cell(0, Vector2i(x, y), 0, atlas)
 
 func _spawn_player():
 	player.position = Vector2(generator.player_pos.x * TILE_SIZE + TILE_SIZE / 2, 
